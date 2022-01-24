1897da15bea9bbee89d40688de8e0e0bac836026
dganai <dinaganai@gmail.com>
2022-01-24 18:27:50 +0000

    adding additional endpoint

--- express_server.js
+++ express_server.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// adding additional endpoints
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
