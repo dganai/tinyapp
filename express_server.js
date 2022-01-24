1a0b47326e437412e36935102f5e8845c2af25c7
dganai <dinaganai@gmail.com>
2022-01-24 22:04:02 +0000

    implement generateRandomString() for form submission

--- express_server.js
+++ express_server.js
@@ -5,14 +5,28 @@ const PORT = 8080; // default port
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+// generate random strings for a user ID
+  const characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let randomStr = "";
+  for (let i = 0; i < 6; i++) {
+    // random number between 0 and number of alphanumeric chars
+    let randomNum = Math.floor(Math.random() * characters.length);
+    randomStr += characters[randomNum];
+
+  }
+  return randomStr;
+}
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -24,18 +38,25 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+
+// form submission
+app.post("/urls", (req, res) => {
+let shortURL = generateRandomString(); // creating random short url and add to URL database then redirect
+urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`);
 })
-
-
 
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+})
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -51,16 +72,9 @@ app.get("/hello", (req, res) => {
 
 
 
-app.post("/urls", (req, res) => {
 
-  console.log(req.body);
-  res.send("Ok");
-})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
-
-}
